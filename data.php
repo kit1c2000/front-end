b9183db1d4c01a4c2a838b71c9485e44bdc408a6
Simon Walker <github@stwalkerster.co.uk>
2014-02-17 22:46:04 +0000

    use query from SQL-Scripts/get month data for vw_schoolscontrpertopic.sql

--- data.php
+++ data.php
@@ -2,7 +2,7 @@
 require_once("config.inc.php");
 
 $db = new PDO("mysql:host=$dbhost;dbname=$dbname;", $dbuser, $dbpass);
-$query = $db->prepare("select * from globalgeodata where longitude > -3.3 and longitude < -3 and latitude < 56 and latitude > 55.8;");
+$query = $db->prepare("SELECT scpt.*, dates.date1 FROM vw_schoolscontrpertopic scpt JOIN (SELECT  m1 AS date1 FROM (SELECT ((SELECT MIN(StartDate) FROM vw_schoolscontrpertopic) - INTERVAL DAYOFMONTH((SELECT  MAX(EndDate) FROM vw_schoolscontrpertopic)) - 1 DAY) + INTERVAL m MONTH AS m1 FROM (SELECT  @ROWNUM:=@ROWNUM + 1 AS m FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t1, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t2, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t3, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t4, (SELECT @ROWNUM:=- 1) t0) d1) d2 WHERE m1 <= (SELECT MAX(EndDate) FROM vw_schoolscontrpertopic) ORDER BY m1) dates WHERE dates.date1 BETWEEN scpt.StartDate AND scpt.EndDate ORDER BY scpt.TopicId, dates.date1;");
 $query->execute();
 $data = $query->fetchAll(PDO::FETCH_ASSOC);
 
