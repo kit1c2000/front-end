1e4bd16d3d7d0a2020bae486ed96048e745dc2ea
Simon Walker <github@stwalkerster.co.uk>
2014-02-20 02:34:38 +0000

    few more queries

--- data.php
+++ data.php
@@ -19,10 +19,12 @@
 require_once("config.inc.php");
 
 // QUERY LIST HERE!
-$queryVariables = array("schoolscontribpertopic", "debug");
+$queryVariables = array("schoolscontribpertopic", "debug", "hwgrants", "allgrants");
 
 $schoolscontribpertopic = "SELECT scpt.*, dates.date1 FROM vw_schoolscontrpertopic scpt JOIN (SELECT  m1 AS date1 FROM (SELECT ((SELECT MIN(StartDate) FROM vw_schoolscontrpertopic) - INTERVAL DAYOFMONTH((SELECT  MAX(EndDate) FROM vw_schoolscontrpertopic)) - 1 DAY) + INTERVAL m MONTH AS m1 FROM (SELECT  @ROWNUM:=@ROWNUM + 1 AS m FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t1, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t2, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t3, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t4, (SELECT @ROWNUM:=- 1) t0) d1) d2 WHERE m1 <= (SELECT MAX(EndDate) FROM vw_schoolscontrpertopic) ORDER BY m1) dates WHERE dates.date1 BETWEEN scpt.StartDate AND scpt.EndDate ORDER BY scpt.TopicId, dates.date1;";
 $debug = "SELECT 'sekrit' AS secretSitePassword FROM dual;"; // little easter egg... since http://is.gd/9HluJs got reverted :(
+$hwgrants = "SELECT * FROM vw_hw_grants;";
+$allgrants = "SELECT GrantRefNumber, GrantTitle, TotalGrantValue, HoldingOrganisationName FROM information;";
 // END OF QUERY LIST!
 
 // default to sane value
